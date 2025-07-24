ea61d5299656385a147aec161c4bbaf814e1942c
hsin hen <33472887+hsinhen@users.noreply.github.com>
2025-07-24 10:42:44 +0800

    Merge pull request #1 from hsinhen/features/rolebased-and-registervalidation
    Option 1: Role-Based Routing

--- lib/features/auth/auth_bloc.dart
+++ lib/features/auth/auth_bloc.dart
@@ -1,29 +1,59 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'user_model.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
     on<AppStarted>((event, emit) async {
-      await Future.delayed(const Duration(seconds: 1));
-      emit(Unauthenticated());
-    });
-
-    on<LoginRequested>((event, emit) async {
       emit(AuthLoading());
-      await Future.delayed(const Duration(seconds: 1));
-
-      // Fake auth logic: accept anything
-      if (event.email.isNotEmpty && event.password.isNotEmpty) {
-        emit(Authenticated(User(email: event.email)));
+      final prefs = await SharedPreferences.getInstance();
+      final email = prefs.getString('email');
+      final name = prefs.getString('name');
+      final token = prefs.getString('token');
+      final role = prefs.getString('role');
+      await Future.delayed(Duration(seconds: 1));
+      if (email != null && name != null && token != null && role != null) {
+        emit(Authenticated(User(email: email, name: name, role: role, token: token)));
       } else {
         emit(Unauthenticated());
       }
     });
 
+    on<LoginRequested>((event, emit) async {
+      print('testoooo even.email ${event.email}');
+
+      emit(AuthLoading());
+      await Future.delayed(Duration(seconds: 1));
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('email', event.email);
+      await prefs.setString('name', 'User');
+      await prefs.setString('token', 'token_123');
+      await prefs.setString('role', event.email == 'admin' ? 'admin' : 'user');
+
+      emit(
+        Authenticated(
+          User(email: event.email, name: event.email == 'admin' ? 'Admin' : 'User', role: event.email == 'admin' ? 'admin' : 'user', token: 'token_123'),
+        ),
+      );
+    });
+
     on<LogoutRequested>((event, emit) async {
       emit(AuthLoading());
-      await Future.delayed(const Duration(milliseconds: 500));
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.clear();
+      await Future.delayed(Duration(milliseconds: 500));
       emit(Unauthenticated());
+    });
+
+    on<RegisterRequested>((event, emit) async {
+      emit(AuthLoading());
+      await Future.delayed(Duration(seconds: 1));
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('email', event.email);
+      await prefs.setString('name', event.name);
+      await prefs.setString('token', 'registered_token');
+      await prefs.setString('role', event.role);
+      emit(Authenticated(User(email: event.email, name: event.name, role: event.role, token: 'registered_token')));
     });
   }
 }
@@ -41,10 +71,17 @@ class LoginRequested extends AuthEvent {
 
 class LogoutRequested extends AuthEvent {}
 
+class RegisterRequested extends AuthEvent {
+  final String name, email, password, role;
+  RegisterRequested({required this.name, required this.email, required this.password, required this.role});
+}
+
 // ----------------------------- STATES -----------------------------
 abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
 
 class Authenticated extends AuthState {
   final User user;
@@ -52,5 +89,3 @@ class Authenticated extends AuthState {
 }
 
 class Unauthenticated extends AuthState {}
-
-class AuthLoading extends AuthState {}
--- lib/features/auth/user_model.dart
+++ lib/features/auth/user_model.dart
@@ -3,6 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class User {
   final String email;
+  final String name;
+  final String role;
   final String token;
-  const User({required this.email, required this.token});
+
+  User({required this.email, required this.name, required this.role, required this.token});
 }
